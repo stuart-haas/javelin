0c1fe2d8b7e620ef3a82d41a6de985785ce6ca1b
Stuart Haas <stuart.haas@fusionary.com>
2021-03-17 02:42:09 -0400

    Add time and time range

--- client/src/views/pages/trackers/Tracker.vue
+++ client/src/views/pages/trackers/Tracker.vue
@@ -1,117 +1,124 @@
 <template>
-  <div class="box">
-    <div class="level">
-      <div class="level-left">
-        <div class="level-item is-clickable" @click.stop="handleEdit('name')">
-          <div>
-            <p class="is-size-7 has-text-weight-bold">Name</p>
-            <span v-show="!edit['name']">{{ tracker.name }}</span>
-            <div v-show="edit['name']" class="field">
+  <div class="card mb-5">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ date }} {{ tracker.complete && `&vert; ${timeRange}` }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item is-clickable" @click.stop="handleEdit('name')">
+            <div>
+              <p class="is-size-7 has-text-weight-bold">Name</p>
+              <span v-show="!edit['name']">{{ tracker.name }}</span>
+              <div v-show="edit['name']" class="field">
+                <p class="control">
+                  <input
+                    ref="name"
+                    type="text"
+                    class="input"
+                    v-model="formData.name"
+                    :placeholder="tracker.name"
+                    @keydown.enter="handleUpdate('name')"
+                    @keyup.esc="handleCancel('name')"
+                    @blur="handleUpdate('name')"
+                    @keydown.tab.prevent="handleNext('name', 'project')"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="level-item ml-6 is-clickable"
+            @click.stop="handleEdit('project')"
+          >
+            <div>
+              <p class="is-size-7 has-text-weight-bold">Project</p>
+              <span v-show="!edit['project']">{{ tracker.project }}</span>
+              <div v-show="edit['project']" class="field">
+                <p class="control">
+                  <input
+                    ref="project"
+                    type="text"
+                    class="input"
+                    v-model="formData.project"
+                    :placeholder="tracker.project"
+                    @keydown.enter="handleUpdate('project')"
+                    @keyup.esc="handleCancel('project')"
+                    @blur="handleUpdate('project')"
+                    @keydown.tab.prevent="handleNext('project', 'rate')"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item is-clickable" @click.stop="handleEdit('rate')">
+            <div>
+              <p class="is-size-7 has-text-weight-bold">Rate</p>
+              <span v-show="!edit['rate']">{{ tracker.rate }}</span>
+              <div v-show="edit['rate']" class="field">
+                <p class="control">
+                  <input
+                    ref="rate"
+                    type="text"
+                    class="input"
+                    v-model="formData.rate"
+                    :placeholder="tracker.rate"
+                    @keydown.enter="handleUpdate('rate')"
+                    @keyup.esc="handleCancel('rate')"
+                    @blur="handleUpdate('rate')"
+                    @keydown.tab.prevent="handleNext('rate', 'time')"
+                  />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div
+            class="level-item is-clickable ml-4"
+            @click.stop="handleEdit('time')"
+          >
+            <span v-show="!edit['time'] && !tracker.complete">{{ time }}</span>
+            <span v-show="!edit['time'] && tracker.complete">{{
+              formData.time
+            }}</span>
+            <div v-show="edit['time']" class="field">
               <p class="control">
                 <input
-                  ref="name"
+                  ref="time"
                   type="text"
                   class="input"
-                  v-model="formData.name"
-                  :placeholder="tracker.name"
-                  @keydown.enter="handleUpdate('name')"
-                  @keyup.esc="handleCancel('name')"
-                  @blur="handleUpdate('name')"
-                  @keydown.tab.prevent="handleNext('name', 'project')"
+                  placeholder="00:00:00"
+                  v-model="formData.time"
+                  @keydown.enter="handleSetTime"
+                  @keyup.esc="handleCancel('time')"
+                  @blur="handleCancel('time')"
                 />
               </p>
             </div>
           </div>
-        </div>
-        <div
-          class="level-item ml-6 is-clickable"
-          @click.stop="handleEdit('project')"
-        >
-          <div>
-            <p class="is-size-7 has-text-weight-bold">Project</p>
-            <span v-show="!edit['project']">{{ tracker.project }}</span>
-            <div v-show="edit['project']" class="field">
-              <p class="control">
-                <input
-                  ref="project"
-                  type="text"
-                  class="input"
-                  v-model="formData.project"
-                  :placeholder="tracker.project"
-                  @keydown.enter="handleUpdate('project')"
-                  @keyup.esc="handleCancel('project')"
-                  @blur="handleUpdate('project')"
-                  @keydown.tab.prevent="handleNext('project', 'rate')"
-                />
-              </p>
+          <div v-if="!tracker.complete" class="level-item ml-4">
+            <span class="icon is-clickable">
+              <Icon v-if="!running" icon="play" @click="resume" />
+              <Icon v-else icon="pause" @click="pause" />
+            </span>
+          </div>
+          <div v-if="!tracker.complete" class="level-item ml-4">
+            <div class="buttons">
+              <div class="button is-primary" @click="handleComplete">Log</div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="level-right">
-        <div class="level-item is-clickable" @click.stop="handleEdit('rate')">
-          <div>
-            <p class="is-size-7 has-text-weight-bold">Rate</p>
-            <span v-show="!edit['rate']">{{ tracker.rate }}</span>
-            <div v-show="edit['rate']" class="field">
-              <p class="control">
-                <input
-                  ref="rate"
-                  type="text"
-                  class="input"
-                  v-model="formData.rate"
-                  :placeholder="tracker.rate"
-                  @keydown.enter="handleUpdate('rate')"
-                  @keyup.esc="handleCancel('rate')"
-                  @blur="handleUpdate('rate')"
-                  @keydown.tab.prevent="handleNext('rate', 'time')"
-                />
-              </p>
-            </div>
+          <div v-if="tracker.complete" class="level-item ml-4">
+            <span>{{ total }}</span>
           </div>
-        </div>
-        <div
-          class="level-item is-clickable ml-4"
-          @click.stop="handleEdit('time')"
-        >
-          <span v-show="!edit['time'] && !tracker.complete">{{ time }}</span>
-          <span v-show="!edit['time'] && tracker.complete">{{
-            formData.time
-          }}</span>
-          <div v-show="edit['time']" class="field">
-            <p class="control">
-              <input
-                ref="time"
-                type="text"
-                class="input"
-                placeholder="00:00:00"
-                v-model="formData.time"
-                @keydown.enter="handleSetTime"
-                @keyup.esc="handleCancel('time')"
-                @blur="handleCancel('time')"
-              />
-            </p>
+          <div class="level-item ml-4">
+            <button
+              class="delete has-background-danger"
+              @click="handleRemove"
+            ></button>
           </div>
-        </div>
-        <div v-if="!tracker.complete" class="level-item ml-4">
-          <span class="icon is-clickable">
-            <Icon v-if="!running" icon="play" @click="resume" />
-            <Icon v-else icon="pause" @click="pause" />
-          </span>
-        </div>
-        <div v-if="!tracker.complete" class="level-item ml-4">
-          <div class="buttons">
-            <div class="button is-primary" @click="handleComplete">Log</div>
-          </div>
-        </div>
-        <div v-if="tracker.complete" class="level-item ml-4">
-          <span>{{ total }}</span>
-        </div>
-        <div class="level-item ml-4">
-          <button
-            class="delete has-background-danger"
-            @click="handleRemove"
-          ></button>
         </div>
       </div>
     </div>
@@ -121,6 +128,7 @@
 <script>
 import ClickOutside from 'vue-click-outside';
 import { timerMixin } from '../../../mixins/timer-mixin';
+import * as timeago from 'timeago.js';
 
 export default {
   mixins: [timerMixin],
@@ -142,6 +150,9 @@ export default {
     };
   },
   computed: {
+    date() {
+      return timeago.format(this.formData.createdAt);
+    },
     timeId() {
       return `${this.tracker._id}-time`;
     },
@@ -150,6 +161,41 @@ export default {
     },
     total() {
       return `$${this.formData.rate * this.timeAsHours}`;
+    },
+    timeRange() {
+      const start = new Date(this.formData.createdAt);
+      const startHours =
+        start.getHours() < 10 ? `0${start.getHours()}` : start.getHours();
+      const startMinutes =
+        start.getMinutes() < 10 ? `0${start.getMinutes()}` : start.getMinutes();
+
+      const endHours = Number(
+        this.formData.time && this.formData.time.split(':')[0]
+      );
+      const endMinutes = Number(
+        this.formData.time && this.formData.time.split(':')[1]
+      );
+
+      console.log(endHours, endMinutes);
+
+      const diffHours = Math.abs(endHours + Number(start.getHours()));
+      const diffMinutes = Math.abs(endMinutes + Number(start.getMinutes()));
+
+      console.log(diffHours, diffMinutes);
+
+      const finalEndHours = diffHours < 10 ? `0${diffHours}` : diffHours;
+      const finalEndMinutes =
+        diffMinutes < 10 ? `0${diffMinutes}` : diffMinutes;
+
+      return (
+        startHours +
+        ':' +
+        startMinutes +
+        '-' +
+        finalEndHours +
+        ':' +
+        finalEndMinutes
+      );
     },
     timeAsHours() {
       const milliseconds =
